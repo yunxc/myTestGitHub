f93b38cda47353945d213e5b31a0f7c2a721b541
yunlong.zhang <58323+yunxc@users.noreply.github.com>
2019-01-04 10:29:22 +0800

    Initial commit

---
+++ src/main/java/testGit/Main1.java
@@ -0,0 +1,11 @@
+package testGit;
+
+/**
+ * @author yunlong.zhang
+ * @date 2019/1/4 10:23
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
