b59691c8c39f9dab8e9166c202a2b3ee5229acff
yun <44458323+yunxc@users.noreply.github.com>
2019-02-19 17:59:30 +0800

    添加

---
+++ src/main/java/com/yun/copy/Main1.java
@@ -0,0 +1,11 @@
+package com.yun.copy;
+
+/**
+ * @author yunlong.zhang
+ * @date 2019/1/30 12:05
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        Node1 nide1 = new Node1();
+    }
+}
---
+++ src/main/java/com/yun/copy/Node1.java
@@ -0,0 +1,26 @@
+package com.yun.copy;
+
+/**
+ * @author yunlong.zhang
+ * @date 2019/1/30 12:06
+ */
+public class Node1 {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
